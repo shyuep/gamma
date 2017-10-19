96c4458ce191a94163bace5a56941d0d80684fc3
__sp__ <ongsp@eng.ucsd.edu>
2017-10-19 15:35:41 -0700

    More flexible query.

--- gamma/data.py
+++ gamma/data.py
@@ -17,9 +17,11 @@ class AlphaVantage:
             else os.environ["ALPHA_VANTAGE_APIKEY"]
         self.session = requests.Session()
 
-    def get_daily_data(self, symbol, fmt="df", **kwargs):
+    def get_time_series_data(self, symbol, series_type="daily", fmt="df",
+                             **kwargs):
         """
         :param symbol:
+        :param series_type: DAILY, WEEKLY or MONTHLY
         :param fmt:
         :param kwargs: These are passthrough as additional keywords to control
             the output. E.g., outputsize="full" provides 20 year data rather
@@ -27,7 +29,7 @@ class AlphaVantage:
         :return:
         """
         payload = {
-            "function": "TIME_SERIES_DAILY",
+            "function": "TIME_SERIES_%s" % series_type.upper(),
             "symbol": symbol,
             "apikey": self.api_key
         }
@@ -44,15 +46,21 @@ class AlphaVantage:
             # {'2017-10-19': {'1. open': '234.0300', '2. high': '234.9900',
             # '3. low': '233.6200', '4. close': '234.9900',
             # '5. volume': '1653619'}
+
+            timeseries = None
+
+            for k, v in raw.items():
+                if k != 'Meta Data':
+                    timeseries = v
+                    break
             index = []
             data = []
-            columns = []
-            for k, v in raw['Time Series (Daily)'].items():
+            for k, v in timeseries.items():
                 date = datetime.strptime(k, "%Y-%m-%d")
                 index.append(date)
                 row = [float(v[k2]) for k2 in sorted(v.keys())]
-                columns = [s.split(".")[-1].strip() for s in sorted(v.keys())]
                 data.append(row)
+            columns = [s.split(".")[-1].strip() for s in sorted(v.keys())]
             return pd.DataFrame(data, index=index, columns=columns)
 
         raise ValueError("REST query returned with error status code {}"
@@ -66,8 +74,14 @@ class AlphaVantageTest(unittest.TestCase):
 
     def test_get_daily_data(self):
         source = AlphaVantage()
-        df = source.get_daily_data("VOO")
-        self.assertEqual(len(df), 100)
+        df = source.get_time_series_data("VOO", outputsize="full")
+        self.assertGreater(len(df), 1000)
+        weeks = len(df) / 5
+        mths = len(df) / 30
+        df = source.get_time_series_data("VOO", series_type="WEEKLY")
+        self.assertTrue(abs(len(df) - weeks) < 100)
+        df = source.get_time_series_data("VOO", series_type="MONTHLY")
+        self.assertTrue(abs(len(df) - mths) < 30)
 
 
 if __name__ == "__main__":
